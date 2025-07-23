d70878ccb19fba796f19e6f2353eccd556de84f1
Narciso <Narciso@DESKTOP-AALVT57>
2025-07-23 13:59:34 -0400

    Clase ProyectoSoftware creada y subida. Clase ColaboradorTech modificada

--- ProyectoFinal/src/logico/ColaboradorTech.java
+++ ProyectoFinal/src/logico/ColaboradorTech.java
@@ -1,26 +1,37 @@
 package logico;
 
-public class ColaboradorTech {
-	private String id;
-    private String nombre;
-    private String apellidos;
-    private String email;
-    private char sexo;
-    private int edad;
-    private String fechaIngreso;
-    private String proyectoAsignado;
+public abstract class ColaboradorTech {
+	protected String id;
+    protected String nombre;
+    protected String apellido;
+    protected String direccion;
+    protected char sexo;
+    protected int edad;
+    protected double salario;
+    protected String proyectoAsignado;
     
-    public ColaboradorTech(String id, String nombre, String apellidos, String email, 
-            char sexo, int edad, String fechaIngreso, String proyectoAsignado) {
-    	
+    public ColaboradorTech(String id, String nombre, String apellido, String direccion, 
+                          char sexo, int edad, double salario, String proyectoAsignado) {
         this.id = id;
         this.nombre = nombre;
-        this.apellidos = apellidos;
-        this.email = email;
+        this.apellido = apellido;
+        this.direccion = direccion;
         this.sexo = sexo;
         this.edad = edad;
-        this.fechaIngreso = fechaIngreso;
+        this.salario = salario;
         this.proyectoAsignado = proyectoAsignado;
-  }
+    }
+    
 
+    public String getId() { return id; }
+    public String getNombre() { return nombre; }
+    public String getApellidos() { return apellido; }
+    public String getDireccion() { return direccion; }
+    public char getSexo() { return sexo; }
+    public int getEdad() { return edad; }
+    public double getSalario() { return salario; }
+    public String getProyectoAsignado() { return proyectoAsignado; }
+    
+    
+    public abstract String mostrarResumen();
 }
