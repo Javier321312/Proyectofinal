2fa4361351fa62c303ebfb65683ddc574668dcd8
Javier Lopez <JDLC0002@ce.pucmm.edu.do>
2025-07-23 20:03:03 -0400

    Update Cliente.java
    Clase cliente terminada

--- ProyectoFinal/src/logico/Cliente.java
+++ ProyectoFinal/src/logico/Cliente.java
@@ -1,5 +1,27 @@
 package logico;
 
 public class Cliente {
-
+private String nombre;
+	    private String contacto;
+	    
+	    public Cliente(String nombre, String contacto) {
+	        this.nombre = nombre;
+	        this.contacto = contacto;
+	    }
+	    
+	    public String agregarProyecto(String nombreProyecto) {
+	        return "Proyecto '" + nombreProyecto + "' agregado para cliente: " + nombre;
+	    }
+	    
+	    public String getCalidadProyectos() {
+	        return "Evaluando calidad de proyectos para cliente: " + nombre;
+	    }
+	    
+	    public String getDatosCliente() {
+	        return String.format("Cliente: %s - Contacto: %s", nombre, contacto);
+	    }
+	    
+	 
+	    public String getNombre() { return nombre; }
+	    public String getContacto() { return contacto; }
 }
